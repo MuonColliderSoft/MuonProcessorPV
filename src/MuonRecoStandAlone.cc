5cfc18b02cb63d7fe726bf7f59c5b33dda60f568
caime <caime@cern.ch>
2023-12-11 18:31:04 +0100

    reduced number of hits

--- src/MuonRecoStandAlone.cc
+++ src/MuonRecoStandAlone.cc
@@ -233,7 +233,7 @@ void MuonRecoStandAlone::processEvent( LCEvent* evt ) {
     for (unsigned int icol=0; icol<inputHitColls.size(); ++icol){
       LCCollection* hit_col  =  inputHitColls[icol];
       if( !hit_col ) continue ;
-      int numerohit_check = 0; //AGGIUNTA GROSSO RISCHIO
+      //int numerohit_check = 0; //AGGIUNTA GROSSO RISCHIO
       for (int ihit=0; ihit<hit_col->getNumberOfElements(); ++ihit){
 	TrackerHitPlane* hitplane = dynamic_cast<TrackerHitPlane*>(hit_col->getElementAt(ihit));
 	
@@ -242,7 +242,7 @@ void MuonRecoStandAlone::processEvent( LCEvent* evt ) {
 	
 	float deltaR_trackerhit=trackerhit.DeltaR(cl_center);
 	if ( deltaR_trackerhit < m_cutdRTracker){
-	  numerohit_check = numerohit_check +1;
+	  //numerohit_check = numerohit_check +1;
 	  TrackerHitPlaneImpl* hitplane_new = new TrackerHitPlaneImpl();
 	  hitplane_new->setCellID0(hitplane->getCellID0());
 	  hitplane_new->setCellID1(hitplane->getCellID1());
@@ -261,10 +261,10 @@ void MuonRecoStandAlone::processEvent( LCEvent* evt ) {
 	  hitplane_new->setdV(hitplane->getdV());
 	  //hitplane_new->setOverlay(hitplane->isOverlay());
 	  // hitplane_new->setProducedBySecondary(hitplane->isProducedBySecondary());
-	  if (numerohit_check<30) outputTrackerHitColls[icol]->addElement(hitplane_new);
+	  if (outputTrackerHitColls[icol]->size()<30) outputTrackerHitColls[icol]->addElement(hitplane_new);
 	  }
       } // ihit loop  
-      std::cout << "numero hit check " <<   numerohit_check  << std::endl;
+      //std::cout << "numero hit check " <<   numerohit_check  << std::endl;
     } // icol loop
     } //close if trk_flag
   } //end loop hits on first layer
